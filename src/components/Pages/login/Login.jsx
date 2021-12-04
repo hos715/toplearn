b4c4eb3bfd2bf815b4aeaa52e06f42ab62d542d9
HosseinKhalili <45583045+hos715@users.noreply.github.com>
2021-12-04 18:59:32 +0330

    handle login session with axios also use local-storage for login proccess

--- src/components/Pages/login/Login.jsx
+++ src/components/Pages/login/Login.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { toast } from 'react-toastify';
+import { useState } from 'react/cjs/react.development';
+import { loginUser } from '../../../services/userService';
 
 const Login = () => {
+
+   const navigate = useNavigate();
+
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   const resetStates = () => {
+      setEmail("");
+      setPassword("");
+   }
+
+   const handleLoginUser = async event => {
+      event.preventDefault();
+      const signinUser = {
+         email,
+         password
+      }
+      try {
+         const { status, data } = await loginUser(signinUser)
+         if (status === 200) {
+            toast.success("ورود موفقیت امیز بود.")
+            localStorage.setItem('token', data.token);
+            resetStates();
+            navigate('/', { replace: true });
+         }
+      } catch (ex) { console.log(ex); }
+   }
+
    return (
       <>
          <div className="container">
@@ -18,16 +51,30 @@ const Login = () => {
 
                <div className="form-layer">
 
-                  <form action="" method="">
+                  <form onSubmit={e => handleLoginUser(e)}>
 
                      <div className="input-group">
                         <span className="input-group-addon" id="email-address"><i className="zmdi zmdi-email"></i></span>
-                        <input type="text" className="form-control" placeholder="ایمیل" aria-describedby="email-address" />
+                        <input
+                           type="email"
+                           className="form-control"
+                           placeholder="ایمیل"
+                           aria-describedby="email-address"
+                           value={email}
+                           onChange={e => setEmail(e.target.value)}
+                        />
                      </div>
 
                      <div className="input-group">
                         <span className="input-group-addon" id="password"><i className="zmdi zmdi-lock"></i></span>
-                        <input type="text" className="form-control" placeholder="رمز عبور " aria-describedby="password" />
+                        <input
+                           type="password"
+                           className="form-control"
+                           placeholder="رمز عبور"
+                           aria-describedby="password"
+                           value={password}
+                           onChange={e => setPassword(e.target.value)}
+                        />
                      </div>
 
                      <div className="remember-me">
